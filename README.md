e7c2071be28b69985c6212f3f555b0c0e020be20
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-02-19 20:32:46 +0100

    uppdaterat readme och klar för inskick

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-Laboration 3
+Laboration 3 och 4
 
 Länk till min publicerade sida:
 
 https://labb-3.onrender.com/
+
+(Laboration 3)
+I denna laboration har jag fokuserat på att lära mig SASS:
+
+Exempel på saker jag gjort:
+
+Använt @use för att dela upp koden i bland annat \_base.scss, \_layout.scss och \_animations.scss.
+Använt %card-style och %button-style för att återanvända kod på ett effektivt sätt.
+Skapat egna mixins för att hantera bilder och logik för t.ex. runda hörn.
+Lagrat färger och typsnitt i variabler
+
+(Laboration 4)
+Jag har skapat tre olika typer av animationer:
+
+Rörliga objekt: En Minion-bild som rör sig i ett svävande mönster med hjälp av @keyframes och transform: translate.
+
+Bildanimation (Hover): Fotogalleriet använder transform: scale och transition för att skapa en mjuk inzoomningseffekt när användaren håller musen över bilderna.
+
+Användarinteraktion: En "Konfetti-knapp" som triggar ett konfettiregn. Detta är byggt med JavaScript som kommunicerar med biblioteket canvas-confetti.
--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,29 @@
 "use strict";
 
 /* hamburgermeny */
-const hamburger = document.getElementById('hamburger');
-const navMeny = document.getElementById('nav-meny');
+const hamburger = document.getElementById("hamburger");
+const navMeny = document.getElementById("nav-meny");
 
 if (hamburger && navMeny) {
-    hamburger.addEventListener('click', () => {
-        navMeny.classList.toggle('active');
-    });
+  hamburger.addEventListener("click", () => {
+    navMeny.classList.toggle("active");
+  });
 }
 
+//Konfettiknapp från youtube, har modifierat och förenklat koden till min nybörjarnivå: https://www.youtube.com/watch?v=H3CqghZkHm8
 
-//Konfettiknapp från youtube, har modifierat och förenklat koden till min nybörjarnivå: https://www.youtube.com/watch?v=H3CqghZkHm8  
-
-document.addEventListener("DOMContentLoaded", function() {
-  
-  document.querySelectorAll(".btn-fun").forEach(button => {
-      button.addEventListener("click", function() { 
-          
-          
-          if (typeof confetti === "function") { //Aktiverar confetti genom att hämta instruktionerna från länken i html 
-              confetti({
-                  particleCount: 300, //Antal konfetti "bitar"
-                  spread: 70, //hur brett ska konfettin spridas
-                  origin: { y: 0.6 }, //Startpunkt = 60% från webbläsarfönstret
-                  colors: ['#f093fb', '#f5576c', '#ffffff'] //Färgerna på konfettin
-              });
-          }
-
-           }); 
-    }); 
-}); 
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".btn-fun").forEach((button) => {
+    button.addEventListener("click", function () {
+      if (typeof confetti === "function") {
+        //Aktiverar confetti genom att hämta instruktionerna från länken i html
+        confetti({
+          particleCount: 300, //Antal konfetti "bitar"
+          spread: 70, //hur brett ska konfettin spridas
+          origin: { y: 0.6 }, //Startpunkt = 60% från webbläsarfönstret
+          colors: ["#f093fb", "#f5576c", "#ffffff"], //Färgerna på konfettin
+        });
+      }
+    });
+  });
+});
